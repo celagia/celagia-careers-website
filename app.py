0aaaf875bf1617a6e0bce92160f2eb809a075804
celagia <36736971+celagia@users.noreply.github.com>
2024-02-08 10:55:59 +0000

    render dynamic data using jinja template tags

--- app.py
+++ app.py
@@ -2,10 +2,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [{
+    'id': 1,
+    'title': 'Mountain Climber',
+    'location': 'Cairngorms',
+    'salary': '£30,000'
+}, {
+    'id': 2,
+    'title': 'Group Hike Leader',
+    'location': 'Lake District',
+    'salary': '£40,000'
+}, {
+    'id': 3,
+    'title': 'Scrambler',
+    'location': 'Snowdonia',
+    'salary': '£50,000'
+}, {
+    'id': 4,
+    'title': 'Car Park Barista',
+    'location': 'Peak District',
+    'salary': '£26,000'
+}]
+
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
 
 
 if __name__ == "__main__":
