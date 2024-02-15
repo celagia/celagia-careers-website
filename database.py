d769028c560a24ba3f7628a84c1bed4c3e5fe59c
celagia <36736971+celagia@users.noreply.github.com>
2024-02-15 16:05:34 +0000

    tested REST API, added elements, added dynamic job pages

--- database.py
+++ database.py
@@ -18,3 +18,13 @@ def load_jobs_from_db():
     for row in result.all():
       jobs.append(dict(row._mapping))
     return jobs
+
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text(f"SELECT * FROM jobs where id = {id}"))
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0]._mapping)
